bc64fd60addc0cbeaab003c335b8d09287913cb5
Carlos Carrillo <carlos.e119@gmail.com>
2026-09-03 14:15:26 -0400

    chore: add official plugin URL

--- bastion-security-wp.php
+++ bastion-security-wp.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Plugin Name:       Bastion Security WP
+ * Plugin URI:        https://github.com/carlose119/bastion-security-wp
  * Description:       Defense-in-depth security diagnostics and reversible hardening tools for WordPress.
  * Version:           0.1.0
  * Requires at least: 6.8
